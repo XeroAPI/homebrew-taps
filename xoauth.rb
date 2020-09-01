b457f5f9267fca4af9a200f568233e9c66bc0b8a
Josh Barr <josh.barr@xero.com>
2020-09-02 11:16:38 +1200

    Brew formula update for xoauth version v1.1.0

--- xoauth.rb
+++ xoauth.rb
@@ -2,16 +2,16 @@
 class Xoauth < Formula
   desc "xoath is a cross-platform tool for obtaining JWTs from OpenId Connect providers"
   homepage "https://github.com/XeroAPI/xoauth"
-  version "1.0.0"
+  version "1.1.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/XeroAPI/xoauth/releases/download/v1.0.0/xoauth_1.0.0_darwin_amd64.tar.gz"
-    sha256 "a5d4eba806d2af108774e167baa0da3104e9fd3baee35623fa59f9fe931835d2"
+    url "https://github.com/XeroAPI/xoauth/releases/download/v1.1.0/xoauth_1.1.0_darwin_amd64.tar.gz"
+    sha256 "3e44e94ebb4fa1522d4af122e4626b4a1c6a2dec7f37a258aaffedf4d74ca69f"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/XeroAPI/xoauth/releases/download/v1.0.0/xoauth_1.0.0_linux_amd64.tar.gz"
-      sha256 "46eb35fe747b7830d04f12cb16722376ff473fd18adc70f62a1f36ed89815913"
+      url "https://github.com/XeroAPI/xoauth/releases/download/v1.1.0/xoauth_1.1.0_linux_amd64.tar.gz"
+      sha256 "cb19a90502ec59990dda7901388d3c2b8a0612d319f042c4b66ed9d9dc59fe54"
     end
   end
 
